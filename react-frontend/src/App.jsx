ded1d7ad66e0771afd4e8c5e53821e5dca63e709
Mamatha Gubbala <mamatha.gubbala@ingka.ikea.com>
2024-09-19 16:17:48 +0200

    create users

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -14,12 +14,47 @@ function App() {
     console.log(usersToJson)
     setUsers(usersToJson);
   };
+
+  const createUser = async (e) => {
+    e.preventDefault()
+    const response = await fetch(`${hostUrl}api/users`, {
+    method: 'POST',
+    headers: {
+        "Content-type": "application/json",
+    },
+    body: JSON.stringify({ 
+      firstname: e.target.firstname.value, 
+      lastname: e.target.lastname.value, 
+      age: e.target.age.value, 
+      Gender: e.target.Gender.value, 
+      isAdmin: e.target.isAdmin.value
+     })
+    });
+    const newUser = await response.json();
+    setUsers([...users, newUser]);
+}
+
   useEffect(() => {
     fetchUsers();
   }, []);
 
   return (
     <>
+      <h1>New User</h1>
+    <form onSubmit={createUser}>
+      <label htmlFor="firstname">firstname</label>
+      <input type="text" name="firstname" id="firstName" />
+      <label htmlFor="lastname">lastname</label>
+      <input type="text" name="lastname" id="lastName" />
+      <label htmlFor="age">age</label>
+      <input type="text" name="age" id="ages" />
+      <label htmlFor="Gender">Gender</label>
+      <input type="text" name="Gender" id="Genders" />
+      <label htmlFor="isAdmin">Is Admin</label>
+      <input type="checkbox" name="isAdmin"/>
+      <input type="submit" />
+    </form>
+
       <h1>Users</h1>
       <table>
         <thead>
